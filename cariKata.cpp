955fb5a155d5077d1afa1347e7bed402e7b358f0
aninditatrimulia <151343448+aninditatrimulia@users.noreply.github.com>
2024-04-27 16:30:06 +0700

    upload matriks

--- cariKata.cpp
+++ cariKata.cpp
@@ -4,33 +4,18 @@
 
 using namespace std;
 
-bool searchDiagonal(vector<string>& matrix, string word) {
-    int n = matrix.size();
-    int m = matrix[0].size();
-
-
-    for (int i = 0; i < n; ++i) {
-        string diagonal;
-        for (int j = 0; i + j < n && j < m; ++j) {
-            diagonal.push_back(matrix[i + j][j]);
-        }
-        if (diagonal.find(word) != string::npos) {
-            return true;
-        }
-    }
-
-
-    for (int j = 1; j < m; ++j) {
-        string diagonal;
-        for (int i = 0; i < n && j + i < m; ++i) {
-            diagonal.push_back(matrix[i][j + i]);
-        }
-        if (diagonal.find(word) != string::npos) {
-            return true;
-        }
-    }
-
-    return false;
+int main() {
+    
+    vector<string> matrix = {
+        "aaflkhpfssuficiclesgnnh","sfvreomrwlrttsxqqnaoao","qeiaifxaeirfvfysximinji",
+		"wstrlgocapbiafiwiwtuacm","feyaeapistpcrlujkoakcers","rvdakpndeehdemsnckkfoah",
+		"mrnedslcrriwnrsaafitmmi","yaaecieahymotavhrsstisb","rjsewelccennietohwsglse",
+		"atanyymoieesnesioireltr","utenewebhmybetnnraieben","rclkuteaeqjlsgshtgdskoa",
+		"bhoicatnrrsddecehoolgit","ensluarirsetalocohctohe","fzfudqjymadoyiwyglovesu",
+		"tekalfwonsnaebmiejtzntg","eswposjxeutuyozuwakezhm","kzuhbpezeerflmsnowballh",
+		"nsnowboardytvwyclevohsa","acocrolgziychodrazzilbi","lbvkkwanzaaqinwolpwonsl",
+		"bfreezingrainslilgtmelt","hqpylwhfmnffufpswxnummv",
+    };
+    
+    return 0;
 }
-
-
